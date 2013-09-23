a9eed2edcb68660161f4a179e18e1d2802feb54a
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-23 16:53:16 +0800

    Implemented multiple taps and designed the UI.

--- Tap Taps/Tap Taps/BIDViewController.h
+++ Tap Taps/Tap Taps/BIDViewController.h
@@ -10,4 +10,14 @@
 
 @interface BIDViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *singleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *doubleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tripleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *quadrupleLabel;
+- (void)tap1;
+- (void)tap2;
+- (void)tap3;
+- (void)tap4;
+- (void)eraseMe:(UILabel *)label;
+
 @end
--- Tap Taps/Tap Taps/BIDViewController.m
+++ Tap Taps/Tap Taps/BIDViewController.m
@@ -9,6 +9,33 @@
 #import "BIDViewController.h"
 
 @implementation BIDViewController
+@synthesize singleLabel;
+@synthesize doubleLabel;
+@synthesize tripleLabel;
+@synthesize quadrupleLabel;
+- (void)tap1 {
+    singleLabel.text = @"Single Tap Detected";
+    [self performSelector:@selector(eraseMe:)
+               withObject:singleLabel afterDelay:1.6f];
+}
+- (void)tap2 {
+    doubleLabel.text = @"Double Tap Detected";
+    [self performSelector:@selector(eraseMe:)
+               withObject:doubleLabel afterDelay:1.6f];
+}
+- (void)tap3 {
+    tripleLabel.text = @"Triple Tap Detected";
+    [self performSelector:@selector(eraseMe:)
+               withObject:tripleLabel afterDelay:1.6f];
+}
+- (void)tap4 {
+    quadrupleLabel.text = @"Quadruple Tap Detected";
+    [self performSelector:@selector(eraseMe:)
+               withObject:quadrupleLabel afterDelay:1.6f];
+}
+- (void)eraseMe:(UILabel *)label {
+    label.text = @"";
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,6 +49,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UITapGestureRecognizer *singleTap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(tap1)];
+    singleTap.numberOfTapsRequired = 1;
+    singleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:singleTap];
+    UITapGestureRecognizer *doubleTap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(tap2)];
+    doubleTap.numberOfTapsRequired = 2;
+    doubleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:doubleTap];
+    [singleTap requireGestureRecognizerToFail:doubleTap];
+    UITapGestureRecognizer *tripleTap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(tap3)];
+    tripleTap.numberOfTapsRequired = 3;
+    tripleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:tripleTap];
+    [doubleTap requireGestureRecognizerToFail:tripleTap];
+    UITapGestureRecognizer *quadrupleTap =
+    [[UITapGestureRecognizer alloc] initWithTarget:self
+                                            action:@selector(tap4)];
+    quadrupleTap.numberOfTapsRequired = 4;
+    quadrupleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:quadrupleTap];
+    [tripleTap requireGestureRecognizerToFail:quadrupleTap];
 }
 
 - (void)viewDidUnload
@@ -29,6 +83,10 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.singleLabel = nil;
+    self.doubleLabel = nil;
+    self.tripleLabel = nil;
+    self.quadrupleLabel = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
